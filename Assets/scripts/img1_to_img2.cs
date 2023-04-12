58b2460660e738efc4baf5f7804ae242f1cc6287
unknown <mariamnadi890@gmail.com>
2023-04-12 16:33:21 +0200

    unity

---
+++ Assets/scripts/img1_to_img2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class img1_to_img2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void movetoImg2()
+    {
+        this.transform.position = new Vector3(5000, 5000, 5000);
+    }
+}
